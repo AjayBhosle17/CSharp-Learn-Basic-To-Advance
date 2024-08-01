38acdd4f2054a511ae59663015c485400a020933
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-08-01 16:17:03 +0530

    Introduction C#

---
+++ 0_Introduction/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+// C# is a programing Language
+// C# is case sensitive Language
+// C# is a developed and maintained by microsoft
+ 
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome in C#");
+
+Console.ReadLine();
