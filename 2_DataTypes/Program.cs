e598ae73d8366341eae23f94b8866550de037c6a
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-08-01 16:18:33 +0530

    Data Types

---
+++ 2_DataTypes/Program.cs
@@ -0,0 +1,148 @@
+﻿// Datatypes
+
+#region Integral DataType
+// Integral Datatypes
+/*
+1) byte
+2) short
+3) int
+4) long*/
+
+// byte
+
+byte x1 = 10;
+
+Console.WriteLine("Byte Data");
+Console.WriteLine($"value of x1 is = {x1}");
+Console.WriteLine($"size of byte is {sizeof(byte)}");
+Console.WriteLine($"Size of min value of byte is = {byte.MinValue} ");
+Console.WriteLine($"Size of max value of byte is = {byte.MaxValue} ");
+
+Console.WriteLine("\n");
+
+
+// short
+
+short x2 = 100;
+
+Console.WriteLine("short Data");
+Console.WriteLine($"value of x2 is = {x2}");
+Console.WriteLine($"size of short is {sizeof(short)}");
+Console.WriteLine($"Size of min value of short is = {short.MinValue} ");
+Console.WriteLine($"Size of max value of short is = {short.MaxValue} ");
+Console.WriteLine("\n");
+
+
+// int
+
+int x3 = 100230;
+
+Console.WriteLine("int Data");
+Console.WriteLine($"value of x3 is = {x3}");
+Console.WriteLine($"size of int is {sizeof(int)}");
+Console.WriteLine($"Size of min value of int is = {int.MinValue} ");
+Console.WriteLine($"Size of max value of int is = {int.MaxValue} ");
+Console.WriteLine("\n");
+
+
+
+// long
+
+long x4 = 1000;
+
+Console.WriteLine("long Data");
+Console.WriteLine($"value of x4 is = {x4}");
+Console.WriteLine($"size of long is {sizeof(long)}");
+Console.WriteLine($"Size of min value of long is = {short.MinValue} ");
+Console.WriteLine($"Size of max value of long is = {short.MaxValue} ");
+Console.WriteLine("\n");
+
+#endregion Integral DataType
+
+#region Floating Datatypes
+// Floating Datatypes
+/*
+1) float
+2) double
+3) decimal
+*/
+
+// float
+
+float x5 = 107.5f;
+
+Console.WriteLine("float Data");
+Console.WriteLine($"value of x5 is = {x5}");
+Console.WriteLine($"size of float is {sizeof(float)}");
+Console.WriteLine($"Size of min value of float is = {float.MinValue}");
+Console.WriteLine($"Size of max value of float is = {float.MaxValue}");
+Console.WriteLine("\n");
+
+
+// double
+
+double x6 = 10237.5;
+
+Console.WriteLine("double Data");
+Console.WriteLine($"value of x6 is = {x6}");
+Console.WriteLine($"size of double is {sizeof(double)}");
+Console.WriteLine($"Size of min value of double is = {double.MinValue}");
+Console.WriteLine($"Size of max value of double is = {double.MaxValue}");
+Console.WriteLine("\n");
+
+
+
+
+// decimal
+
+decimal x7 = 107.5m;
+
+Console.WriteLine("decimal Data");
+Console.WriteLine($"value of x7 is = {x7}");
+Console.WriteLine($"size of decimal is {sizeof(decimal)}");
+Console.WriteLine($"Size of min value of decimal is = {decimal.MinValue}");
+Console.WriteLine($"Size of max value of decimal is = {decimal.MaxValue}");
+Console.WriteLine("\n");
+
+#endregion Floating Datatypes
+
+#region Boolean Datatypes
+// Boolean Datatypes
+// true / false
+
+// float
+
+bool x8 = true;
+
+Console.WriteLine("Boolean Data");
+Console.WriteLine($"value of x8 is = {x8}");
+Console.WriteLine($"size of bool is {sizeof(bool)}");
+Console.WriteLine("\n");
+
+#endregion Boolean Datatypes
+
+#region  String Datatypes
+// String Datatypes
+/*
+1) char
+2) String
+*/
+
+// char
+
+char ch = 'A';
+
+Console.WriteLine("char Data");
+Console.WriteLine($"value of ch is = {ch}");
+Console.WriteLine($"size of char is {sizeof(char)}");
+Console.WriteLine($"Size of min value of char is = {char.MinValue} ");
+Console.WriteLine($"Size of max value of char is = {char.MaxValue} ");
+
+Console.WriteLine("\n");
+
+// String datatype
+
+String name0 = "Ajay Bhosle";
+Console.WriteLine(name0);
+Console.WriteLine("\n");
+#endregion  String Datatypes
