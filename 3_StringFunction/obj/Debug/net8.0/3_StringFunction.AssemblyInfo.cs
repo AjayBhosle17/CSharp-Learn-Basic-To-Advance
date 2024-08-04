09cdf93cc74a65364372d704fdd365b1f26e8712
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-08-04 06:12:23 +0530

    date time

--- 3_StringFunction/obj/Debug/net8.0/3_StringFunction.AssemblyInfo.cs
+++ 3_StringFunction/obj/Debug/net8.0/3_StringFunction.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("3_StringFunction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a34eb1d026e770aac4dc8d336589828ba5d076d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("3_StringFunction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("3_StringFunction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 4_Date_Time/Program.cs
@@ -0,0 +1,25 @@
+﻿// Date
+
+using Microsoft.VisualBasic;
+using System.Reflection.PortableExecutable;
+
+DateTime today = DateTime.Today;
+Console.WriteLine(today);
+
+DateTime current = DateTime.Now;
+Console.WriteLine(current);
+
+String date = current.ToShortDateString();
+Console.WriteLine(date);
+
+String Time = current.ToShortTimeString();
+Console.WriteLine(Time);
+
+String format = current.ToString("dd MMMM yyyy");
+Console.WriteLine(format);
+
+String format1 = current.ToString("MM/dd/yyyy");
+Console.WriteLine(format1);
+
+
+
