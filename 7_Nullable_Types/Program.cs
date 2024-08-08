00be4314cd0c801f8e8f34e8443e9b5416408abd
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-08-08 12:12:34 +0530

    Nullable type and Loop

---
+++ 7_Nullable_Types/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Nullable Type
+
+int num = 10;
+
+Console.WriteLine(num);
+
+// int num1 = null; // Basically by default value of any variable  or datatype is not null.
+
+int? num1 = null;
+
+Console.WriteLine(num1);
+
+int num2 = 20;  // it is a basically non - nullable type
+
+num = num2;
+Console.WriteLine($"Current Value of num is : {num}");
+
+Object obj = ++num; // value type to reference type
+
+Console.WriteLine($"Current Value of obj is : {obj}");
+
+int? num3 = 1200;
+Console.WriteLine(num3);
+
+int num4 = num3 ?? 0;
+Console.WriteLine(num4);
+
+/*String str = null;
+
+Console.WriteLine(str.ToUpper());  // NullReferenceException
+*/
+
+int num6 = (int)obj; // refernce type to value type => Unboxing
+Console.WriteLine(num6);
+
+float num7 = 18.5f;
+
+int num8 = (int)num7;
+Console.WriteLine(num8);
+
---
+++ 8_Loop/Program.cs
@@ -0,0 +1,121 @@
+﻿//Console.WriteLine("Enter No of Rows: ");
+
+//int rows = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 1; i <= rows; i++)
+//{
+
+
+//    for (int j = rows; j > i; j--)
+//    {
+//        Console.Write("  ");
+//    }
+//   int w = i;
+//    for (int k = 1; k <= i; k++)
+//    {
+//        Console.Write($" { w--}");
+
+//    }
+
+//    Console.WriteLine();
+//}
+
+
+//home work 
+//Console.WriteLine("Enter No of Rows: ");
+//int rows = Convert.ToInt32(Console.ReadLine());
+
+//for (int i = 1; i <= rows; i++)
+//{
+//    //Print leading spaces
+//    for (int j = rows; j > i; j--)
+//    {
+//        Console.Write(" ");
+//    }
+
+//    //Print numbers
+//    int w = i;
+//    for (int k = 1; k <= i; k++)
+//    {
+//        Console.Write($"{w--}");
+//    }
+
+//    //Move to the next line
+//    Console.WriteLine();
+//}
+
+
+
+
+
+//Console.WriteLine("Print any");
+
+//char ch = 'a';
+//for (int i = 1; i <= 26; i++)
+//{
+
+//    Console.Write($"{ch++}  ");
+
+//}
+//int a = 1;
+//for (int i = 1; i <= 5; i++)
+
+//{
+//    for (int j = 1; j <= 5; j++) ;
+//    Console.Write( $"*"  );
+//}
+
+
+
+
+
+
+
+//{
+//    Console.WriteLine("Enter the number of rows: ");
+//    int rows = Convert.ToInt32(Console.ReadLine());
+
+//    for (int i = 1; i <= rows; i++)
+//    {
+//        // Print leading spaces
+//        for (int j = rows; j > i; j--)
+//        {
+//            Console.Write(" ");
+//        }
+
+//        // Print numbers starting from the current row number down to 1
+//        for (int k = i; k >= 1; k--)
+//        {
+//            Console.Write(   $"{ k }");
+//        }
+
+//        // Move to the next line
+//        Console.WriteLine();
+//    }
+//}
+
+
+Console.WriteLine("Enter No of Rows");
+
+int rows=Convert .ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= rows; i++) {
+	int k = rows;
+	for (int j = 1; j <= rows; j++)
+	{
+		if (rows - i - j + 1 > 0)
+		{
+
+			Console.Write(" *");
+
+		}
+		else {
+			Console.Write($" {k}");
+		}
+		k--;
+		
+	}
+    Console.WriteLine();
+}
+
+
