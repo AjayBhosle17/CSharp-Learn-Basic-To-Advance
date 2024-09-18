6b7d1679ef49308b2f318a9c26e317d939bfa687
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-09-18 17:58:41 +0530

    HashTable,Stack,Queue in Non generic Collection

--- 44_Collection_NonGeneric/Program.cs
+++ 44_Collection_NonGeneric/Program.cs
@@ -1,11 +1,13 @@
 ﻿
 // non generic
 
-#region ArrayList
+/* **************************************************************** */
 
+#region ArrayList
+/*
 using System.Collections;
 
-/*ArrayList student_Data = new ArrayList();
+*//*ArrayList student_Data = new ArrayList();
 
 Console.WriteLine("Add 5 Student");
 Console.WriteLine("Enter Names: ");
@@ -15,7 +17,7 @@ for (int i = 1; i <= 5; i++) {
     student_Data.Add(Console.ReadLine());
 
 }
-*/
+*//*
 //int[] arr = new int[] { 1,2,3,4};
 //string s = string.Join(" ", arr);
 //Console.WriteLine(s);
@@ -39,10 +41,10 @@ for (int i = 0; i < arrayList.Count; i++) {
     Console.WriteLine();
 }
 //arrayList.Add(10);
-/*foreach (string str in arrayList) {  // InvalidCastException // no type safety
+*//*foreach (string str in arrayList) {  // InvalidCastException // no type safety
                                            
     Console.WriteLine(str);
-}*/
+}*//*
 
 // multiple Item Add
 
@@ -145,10 +147,10 @@ Console.WriteLine("\nIndexOf    public virtual int IndexOf(object? value)\r\n");
 int index = arrayList.IndexOf("Ajay");
 Console.WriteLine($"Found At Index {index}");
 arrayList.Insert(5,"Vijay");
-/*
+*//*
 int index1 = arrayList.IndexOf("Vijay", 0, 2);
 Console.WriteLine($"Found At Index {index1}");
-Console.WriteLine();*/
+Console.WriteLine();*//*
 
 Console.WriteLine("\nPrevious Add Data");
 k = 0;
@@ -215,19 +217,217 @@ Console.WriteLine($"\nClear:    public override void Clear()\r\n");
 arrayList.Clear();
 
 Console.WriteLine("Clear List\n");
+*/
+#endregion
 
+/* **************************************************************** */
+/* **************************************************************** */
 
-arrayList.Add("Ajay");
-arrayList.Add("Vijay");
-arrayList.Add("Om");
-k = 0;
+#region HashTable
 
-foreach (var i in arrayList)
+//key value pair
+
+using System.Collections;
+using System.Security.Cryptography;
+
+Hashtable hashtable = new Hashtable();
+
+Console.WriteLine("\nHashMap Add():   public virtual void Add(object key, object? value)\r\n  ");
+
+hashtable.Add(1,"Ajay");
+hashtable.Add(2, "Vijay");
+hashtable.Add(3, "Om");
+hashtable.Add(-4, "Om");
+
+foreach (DictionaryEntry item in hashtable) { 
+
+    Console.WriteLine($"Key : {item.Key}  Value : {item.Value}");
+}
+
+Console.WriteLine("\nRemove : public virtual void Remove(object key)\r\n");
+hashtable.Remove(3);
+foreach (DictionaryEntry item in hashtable)
 {
 
-    Console.WriteLine($"index: {k++}: {i}");
+    Console.WriteLine($"Key : {item.Key}  Value : {item.Value}");
+}
 
+Console.WriteLine($"\nCount: {hashtable.Count}");
+
+
+// hashtable.Add(3,"Ajay");  // error
+
+
+if (hashtable.ContainsKey(2))
+{
+
+    Console.WriteLine("Present");
+}
+else {
+
+    Console.WriteLine("Not Present");
 }
 
 
+var val = hashtable[1];
+Console.WriteLine(val);
+
+
+ArrayList arrayList = new ArrayList();
+
+arrayList.Add("Ajay");
+arrayList.Add("Vijay");
+
+Console.WriteLine("\nClear : ");
+hashtable.Clear();
+Console.WriteLine("Clear Table");
+
+
+
+/*
+// Question 1 
+// Find Frequency
+
+
+Dictionary<string,int> obj = new Dictionary<string,int>();
+
+foreach (DictionaryEntry item in hashtable) {
+
+    string str = item.Value.ToString();
+    if (obj.ContainsKey(str))
+    {
+
+        obj[str]++;
+    }
+    else { 
+    
+        obj[str]=1;
+    }
+}
+
+
+foreach (var item in obj) {
+
+    Console.WriteLine($"{item.Key}  = {item.Value}");
+}*
+
+
+
+
+
+/* **************************************************************** */
+/* **************************************************************** */
 #endregion
+
+/* **************************************************************** */
+/* **************************************************************** */
+
+#region Stack
+
+//using System.Collections;
+
+
+Stack History = new Stack();
+
+Console.WriteLine("\nPush :    public virtual void Push(object? obj)\r\n");
+History.Push("Tab Click");
+History.Push("Google.com search");
+History.Push("Youtube Search");
+
+foreach (var item in History) { 
+
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\nPop:   public virtual object? Pop()\r\n ");
+
+var pop=History.Pop();
+Console.WriteLine($"Poped : {pop}");
+
+Console.WriteLine("\nPeek: public virtual object? Peek()\r\n");
+var peek = History.Peek();
+Console.WriteLine($"Peek is : {peek}\n");
+
+History.Clear();
+History.Push(10);
+
+History.Push(20);
+
+History.Push(30);
+History.Push("Ajay");
+
+if (History.Contains("Ajay"))
+{
+
+     var temp  = History.Pop();
+    Console.WriteLine($"{temp} : Popped Sucessfully ");
+}
+else {
+
+    History.Push("Ajay");
+    Console.WriteLine("Push Sucessfully");
+}
+
+foreach (var sta in History) { 
+
+    Console.WriteLine(sta);
+}
+//int pop1 = (int)History.Pop();  //  its not work 
+
+// Console.WriteLine(pop1);
+
+
+#endregion Stack
+
+
+/* **************************************************************** */
+/* **************************************************************** */
+
+#region Queue
+
+
+Queue token = new Queue();
+
+Console.WriteLine("\nEnqueue:  public virtual void Enqueue(object? obj)\r\n ");
+token.Enqueue(1);
+token.Enqueue(2);
+token.Enqueue(3);
+Console.WriteLine("Items Are: ");
+foreach (var item in token) { 
+
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("\nDequeue :  public virtual object? Dequeue()\r\n ");
+
+var tk=token.Dequeue();
+Console.WriteLine($"Token Dequeue is : {tk}");
+
+Console.WriteLine("\nNow Items Are: ");
+foreach (var item in token)
+{
+
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Token Count is : {token.Count}");
+
+
+Console.WriteLine($"Peek : public virtual object? Peek()\r\n");  
+tk=token.Peek();
+Console.WriteLine($"Peek Token is {tk}");
+
+Console.WriteLine("\nClear() tokens");
+token.Clear();
+Console.WriteLine("Empty Token");
+
+
+
+
+
+#endregion Queue
+
+
+
+/* **************************************************************** */
+/* **************************************************************** */
--- 44_Collection_NonGeneric/obj/Debug/net8.0/44_Collection_NonGeneric.AssemblyInfo.cs
+++ 44_Collection_NonGeneric/obj/Debug/net8.0/44_Collection_NonGeneric.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("44_Collection_NonGeneric")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1dbd8720597475a33b4f6265b9b0f8f2c96d00f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c552436f8eb4bd4c858aa8b2c6e1a484fd0f7b6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("44_Collection_NonGeneric")]
 [assembly: System.Reflection.AssemblyTitleAttribute("44_Collection_NonGeneric")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
