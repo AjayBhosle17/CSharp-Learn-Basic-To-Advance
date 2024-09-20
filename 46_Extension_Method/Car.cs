becb27d10d1c2e7fde03fec96562d61032a4c0be
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-09-20 13:03:44 +0530

    Extension Method

---
+++ 46_Extension_Method/Car.cs
@@ -0,0 +1,20 @@
+﻿public interface ICar {
+
+}
+ 
+public class Audi : ICar { 
+
+}
+
+public class BMW : ICar { 
+
+}
+
+public static class classHelper {
+
+    public static void print(this ICar a , string CustomerName) {
+
+        Console.WriteLine($"{a.GetType().Name}  Customer_Name = {CustomerName}");
+
+    }
+}
---
+++ 46_Extension_Method/Program.cs
@@ -0,0 +1,47 @@
+﻿
+string name = "Ajay Bhosle";
+name += " AB";
+
+Console.WriteLine(name.ToUpper());
+
+Console.Write(name.MyMethod());
+
+/*public static class StringHelper : string {
+
+    public string MyMethod() {
+
+        return " ";
+    }
+} */
+
+string name2=stringHepler.MyMethod("Ajay");
+Console.WriteLine(name2.ToUpper());
+
+BMW obj2 = new BMW();
+obj2.print("Ajay");
+public static class stringHepler {
+
+    public static string MyMethod(this string name) {
+
+        //return "Mera Method Hai Bro";
+        string Capitalize = "";
+        // Ajay  => aJAY
+        for (int i= 0; i <name.Length; i++)
+        {
+            if (i == 0)
+            {
+
+                Capitalize += name[i].ToString();
+            }
+            else {
+
+                Capitalize += name[i];
+            }
+        }
+        return Capitalize;
+    }
+}
+
+
+
+
